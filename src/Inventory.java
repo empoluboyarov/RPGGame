f7a7eba8c3c9666b628ab91b84e01b43a71cbe77
empoluboyarov <poluboyarovem@gmail.com>
2016-04-12 22:10:16 +0300

    finish new class inventory

--- src/Inventory.java
+++ src/Inventory.java
@@ -5,7 +5,36 @@ import java.util.ArrayList;
  */
 public class Inventory {
 
-    ArrayList<String> inv;
+    private ArrayList<String> inv;
 
-    
+    public Inventory() {
+        inv = new ArrayList<String>();
+    }
+
+    public void add (String newItem){
+        inv.add(newItem);
+    }
+
+    public int getSize(){
+        return inv.size();
+    }
+
+    public String useItem(int itemId){
+        if (itemId == 0)
+            return "";
+        String item = inv.get(itemId-1);
+        inv.remove(itemId-1);
+        return item;
+    }
+
+    public void showAllItems(){
+        System.out.println("Инвентарь: ");
+        if (inv.size()>0){
+            for (int i = 0; i < inv.size(); i++)
+                System.out.println((i+1)+". " + inv.get(i));
+        } else System.out.println("Инвентарь пуст. ");
+
+    }
+
+
 }
